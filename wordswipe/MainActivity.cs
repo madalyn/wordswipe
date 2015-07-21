0c10051a1f83e4efdd1313475b9e2f9bb91b44a3
Madalyn Coryea <madalyn.coryea@xamarin.com>
2015-07-21 12:03:49 -0400

    Setting up wordswipe! May change the name to wordlash

---
+++ wordswipe/MainActivity.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace wordswipe
+{
+	// also no one is using wordlash
+	[Activity (Label = "wordswipe", MainLauncher = true, Icon = "@drawable/icon", Theme = "@style/Theme.Custom")]
+	public class MainActivity : Activity
+	{
+		int count = 1;
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.Main);
+
+			// Get our button from the layout resource,
+			// and attach an event to it
+//			Button button = FindViewById<Button> (Resource.Id.myButton);
+//			
+//			button.Click += delegate {
+//				button.Text = string.Format ("{0} clicks!", count++);
+//			};
+
+
+
+			// on page refresh, set the word & definition
+			var random = new Random();
+			var allWords = new List<string>{"hello", "world", "misanthrope", "sanctimonious", "yellow"};
+			var wordStack = new Stack<string>(allWords.OrderBy(w => random.Next()));
+
+			TextView currentWord = (TextView)FindViewById (Resource.Id.currentWord);
+			currentWord.Text = wordStack.Pop();
+
+			// get definition
+			TextView currentDefinition = (TextView)FindViewById (Resource.Id.definition);
+			currentDefinition.Text = GetCurrentDefinition (currentWord.Text);
+
+			// swiping actions for left & right need to be set
+
+
+			// TODO: menu for list of newly learned words
+		}
+
+		string GetCurrentDefinition (string currentWord)
+		{
+			string currentDefinition = "no definition";
+
+			// use word randomly selected to query for the definition
+			var url = "http://api.wordnik.com:80/v4/word.json/"+ currentWord +"/definitions?limit=1&includeRelated=false&sourceDictionaries=all&useCanonical=false&includeTags=false&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5";
+
+			dynamic jResults = JsonConvert.DeserializeObject (WebRequester.getInstance().doWebRequest(url));
+
+			// want "text" for definition
+			if (jResults != null && jResults[0] != null && jResults[0]["text"] != null && !string.IsNullOrEmpty(jResults[0]["text"].Value)) {
+				currentDefinition = jResults[0]["text"].Value;
+			}
+
+			// definition could not be found
+			return currentDefinition;
+		}
+	}
+}
+
+
---
+++ wordswipe/requests/WebRequester.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Net;
+using System.IO;
+using System.Threading;
+
+namespace wordswipe
+{
+	public class WebRequester
+	{
+		private static WebRequester instance;
+		private int failures = 0;
+
+		private WebRequester()
+		{
+			// the singleton class constuctor
+		}
+
+		// return the WebRequester instance; making one if necessary
+		public static WebRequester getInstance()
+		{
+			if (instance == null)
+			{
+				instance = new WebRequester();
+			}
+
+			return instance;
+		}
+
+		/// <summary>
+		/// perform the web request for the desired url
+		/// </summary>
+		/// <param name="url">the url to make a request to</param>
+		/// <returns>the contents of the response (JSON)</returns>
+		public string doWebRequest(string url)
+		{
+			WebRequest request = WebRequest.Create(url);
+			HttpWebResponse response = null;
+			HttpStatusCode status;
+
+			// get the status, if there is an exception set status
+			try
+			{
+				response = (HttpWebResponse)request.GetResponse();
+				status = response.StatusCode;
+			}
+			catch (Exception e)
+			{
+				status = HttpStatusCode.Forbidden;
+			}
+
+			// if the status is good then read it 
+			// otherwise, try again up to 2 times before giving up
+			if (status == HttpStatusCode.OK)
+			{
+				failures = 0;
+				Stream dataStream = response.GetResponseStream();
+				StreamReader reader = new StreamReader(dataStream);
+				string json = reader.ReadToEnd();
+
+				reader.Close();
+				dataStream.Close();
+				response.Close();
+
+				return json;
+			}
+			else if (failures < 2)
+			{
+				failures++;
+				// it may have failed because of a call limit
+				// wait then try again
+				Thread.Sleep(1000);
+				return doWebRequest(url);
+			}
+			else
+			{
+				failures = 0;
+				// print out appropriate message
+				Console.WriteLine("Could not connect to " + url + "\n\n");
+				return "{}";
+			}
+		}
+	}
+}
+
